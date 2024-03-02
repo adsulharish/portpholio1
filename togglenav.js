7d306aa897e78d4b841708332b10f579071207b8
adsulharish <161346002+adsulharish@users.noreply.github.com>
2024-03-02 19:55:24 +0530

    Add files via upload

---
+++ togglenav.js
@@ -0,0 +1,8 @@
+ function toggleNav() {
+            var nav = document.getElementById("myNav");
+            if (nav.style.left === "0px") {
+                nav.style.left = "-250px";
+            } else {
+                nav.style.left = "0px";
+            }
+        }
